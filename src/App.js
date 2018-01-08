f336236fed8a269649091285a9eae42734471b2e
Paul chabinga <paulchabinga@Pauls-MacBook.local>
2018-01-08 20:55:54 +0000

    created the landing and header page for the application

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <header className="header-section">
+        <div className="header-inner">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8">
+                <h1 className="display-2">NewsWire</h1>
+                <div className="d-flex flex-row">
+                  <div className="p-4 align-self-start">
+                    <i className="fa fa-check" />
+                  </div>
+                  <div className="p-4 align-self-end">
+                    <p>
+                      The best place for you to read all the News in the World
+                    </p>
+                  </div>
+                </div>
+
+                <div className="d-flex flex-row">
+                  <div className="p-4 align-self-start">
+                    <i className="fa fa-check" />
+                  </div>
+                  <div className="p-4 align-self-end">
+                    <p>
+                      We get our News from the best sources in the News business
+                      and it's all for free.
+                    </p>
+                  </div>
+                </div>
+              </div>{" "}
+              {/* End of the col-lg-8 class section */}
+              <div className="col-lg-4">
+                <button className="btn btn-primary btn-lg btn-block">
+                  Get News Now
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     );
   }
 }
