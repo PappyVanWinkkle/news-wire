3f050d5bcfff74f13a810c6975b8b25b397834ce
Paul chabinga <paulchabinga@Pauls-MacBook.local>
2018-01-10 21:42:47 +0000

    made changes to the error 404 page

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -3,24 +3,27 @@
 */
 import React from "react";
 import { Link } from "react-router-dom";
+import "./ErrorPage.css";
 import Nav from "./Navbar";
-
 
 const Error = () => {
   return (
     <div className="jumbotron display-flex flex-row text-center">
       <Nav />
-      <h1 className="display-4">404 ERROR</h1>
-      <p className="lead">
-        Oooops Bad News here please try and go back to the previous page thanks
-      </p>
-      <Link to="/news">
-      <p className="lead">
-        <a className="btn btn-outline-primary" role="button">
-          Back to News
-        </a>
-      </p>
-      </Link>
+      <div className="error-inner">
+        <h1 className="display-4 mt-4">404 ERROR</h1>
+        <p className="lead">
+          Oooops Bad News here please try and go back to the previous page
+          thanks
+        </p>
+        <Link to="/news">
+          <p className="lead">
+            <a className="btn btn-outline-primary" role="button">
+              Back to News
+            </a>
+          </p>
+        </Link>
+      </div>
     </div>
   );
 };
