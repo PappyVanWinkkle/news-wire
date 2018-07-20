92e3f81b4c6b579872fe62c1e0a5fa868897323b
chabz <chabz@chabzs-MacBook.local>
2018-07-20 19:52:07 +0100

    made final changes to the app

--- news/src/index.js
+++ news/src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import Amplify from 'aws-amplify';
+import Config from './aws-exports';
+
+Amplify.configure(Config);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
