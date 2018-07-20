99cfa9b1737b89b376078dbcabdf62bdcc77088a
chabz <chabz@chabzs-MacBook.local>
2018-07-20 22:58:21 +0100

    Made changes to error component

--- news/src/components/errors.js
+++ news/src/components/errors.js
@@ -1,22 +1,32 @@
-/* ========================
-   404 PAGE
-============================ */
+/*
+ ====================
+   404 PAGE FOR THE APP
+ *=================== */
 
 import React from 'react';
-import Nav from './Nav';
 import Footer from './Footer';
+import { Link } from 'react-router-dom';
 
 const Errors = () => {
 	return (
 		<div>
-			<Nav />
-			<div className="row">
-				<div className="col-sm-12">
-					<div className="jumbotron jumbotron-fluid" style={{ height: '100vh', width: '100vw' }}>
-						<div className="container">
-							<h1 className="display-4" style={{ fontWeight: '800' }}>
-								404 Page Not Found
-							</h1>
+			<div className="container-fluid" style={{ background: '#F6F6F6' }}>
+				<div className="row">
+					<div className="col-sm-12">
+						<Link to="/" style={{ textDecoration: 'none' }}>
+							<button type="button" className="btn btn-outline-danger btn-lg mt-4">
+								Back Home
+							</button>
+						</Link>
+						<div>
+							<div className="container">
+								<h1
+									className="display-1 text-center text-danger"
+									style={{ marginTop: '10rem', fontWeight: '800' }}
+								>
+									404 Page Not Found
+								</h1>
+							</div>
 						</div>
 					</div>
 				</div>
